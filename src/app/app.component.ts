3fc1ed01d6fface10123da265bf9bc430bf14dd0
ragavyarasi <ragav.code@gmail.com>
2022-01-11 15:54:35 +0530

    Setup the basic placeholder for the functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'color-words';
+  inputWord = '';
+
+  inputEnter = (event: any) => {
+    if (event.key == 'Enter') {
+      this.searchWord();
+    }
+  };
+  searchWord() {
+    console.log('The word is ', this.inputWord);
+  }
 }
